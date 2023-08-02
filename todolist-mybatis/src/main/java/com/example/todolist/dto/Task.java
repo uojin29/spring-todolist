f5a1bd9681479cf9b167e7af791de64cc3c37721
zionhann <hanzion1108@gmail.com>
2023-08-02 20:49:13 +0900

    Implement basic to-do list app using JdbcTemplate

--- todolist-mybatis/src/main/java/com/example/todolist/dto/Task.java
+++ todolist-mybatis/src/main/java/com/example/todolist/dto/Task.java
@@ -1,7 +1,11 @@
 package com.example.todolist.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Task {
     private Long id;
--- todolist-mybatis/src/main/java/com/example/todolist/repository/JdbcTemplateTaskRepository.java
+++ todolist-mybatis/src/main/java/com/example/todolist/repository/JdbcTemplateTaskRepository.java
@@ -1,35 +1,55 @@
 package com.example.todolist.repository;
 
 import com.example.todolist.dto.Task;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class JdbcTemplateTaskRepository implements TaskRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
     @Override
     public void save(Task task) {
-
+        jdbcTemplate.update(
+                "INSERT INTO task (title) VALUES (?)", task.getTitle()
+        );
     }
 
     @Override
     public Optional<Task> findById(Long id) {
-        return Optional.empty();
+        return jdbcTemplate.query(
+                "SELECT * FROM task WHERE id = ?",
+                BeanPropertyRowMapper.newInstance(Task.class),
+                id).stream().findFirst();
     }
 
     @Override
     public List<Task> findAll() {
-        return null;
+        return jdbcTemplate.query(
+                "SELECT * FROM task",
+                BeanPropertyRowMapper.newInstance(Task.class)
+        );
     }
 
     @Override
     public void update(Task task) {
-
+        jdbcTemplate.update(
+                "UPDATE task SET title = ? WHERE id = ?",
+                task.getTitle(), task.getId()
+        );
     }
 
     @Override
     public void remove(Task task) {
-
+        jdbcTemplate.update(
+                "DELETE FROM task WHERE id = ?", task.getId()
+        );
     }
 }
